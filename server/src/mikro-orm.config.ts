9d755ebc7fa51a4e562d29b3ccbc0643426bbb61
Eduardo Sarmento <cealvesarmento@gmail.com>
2021-05-31 01:59:15 -0300

    Adiciona autenticação de usuário

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -2,16 +2,17 @@ import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 import { MikroORM } from '@mikro-orm/core';
 import path from 'path';
+import { User } from './entities/User';
 
 export default {
   migrations: {
     path: path.join(__dirname, './migrations'),
-    pattern: /^[\w-]+\d+\.ts$/,
+    pattern: /^[\w-]+\d+\.ts$/
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: 'lireddit',
   user: 'postgres',
   password: 'admin',
   type: 'postgresql',
-  debug: !__prod__,
+  debug: !__prod__
 } as Parameters<typeof MikroORM.init>[0];
---
+++ server/src/resolvers/user.ts
@@ -0,0 +1,142 @@
+import {
+  Resolver,
+  InputType,
+  Field,
+  Arg,
+  Ctx,
+  Mutation,
+  ObjectType,
+  Query
+} from 'type-graphql';
+import { MyContext } from '../types';
+import argon2 from 'argon2';
+import { User } from '../entities/User';
+
+@InputType()
+class UserCredentials {
+  @Field()
+  username: string;
+
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { em, req }: MyContext) {
+    if (!req.session.userId) {
+      return null;
+    }
+
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg('options') options: UserCredentials,
+    @Ctx() { em, req }: MyContext
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'Length must be greater than 2'
+          }
+        ]
+      };
+    }
+
+    if (options.password.length <= 2) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'Length must be greater than 2'
+          }
+        ]
+      };
+    }
+
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword
+    });
+
+    try {
+      await em.persistAndFlush(user);
+    } catch (err) {
+      if (err.code === '23505') {
+        return {
+          errors: [
+            {
+              field: 'username',
+              message: 'Username already taken'
+            }
+          ]
+        };
+      }
+    }
+
+    req.session.userId = user.id;
+
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UserCredentials,
+    @Ctx() { em, req }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: "That username doesn't exist"
+          }
+        ]
+      };
+    }
+
+    const valid = await argon2.verify(user.password, options.password);
+
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'Incorrect password'
+          }
+        ]
+      };
+    }
+
+    req.session.userId = user.id;
+
+    return { user };
+  }
+}
